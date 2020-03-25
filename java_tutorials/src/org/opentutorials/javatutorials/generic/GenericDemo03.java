a861e756fcda33163130815e3dfbcd13fc7bac23
kkon999 <gon910910@gmail.com>
2020-03-25 22:59:36 +0900

    ty

--- java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo03.java
+++ java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo03.java
@@ -1,21 +1,12 @@
 package org.opentutorials.javatutorials.generic;
 
-class StudentInfo03{
-    public int grade;
-    StudentInfo03(int grade){ this.grade = grade; }
+
+
+class EmployeeInfo{
+	public int rank;
 }
-class EmployeeInfo03{
-    public int rank;
-    EmployeeInfo03(int rank){ this.rank = rank; }
-}
-class Person03{
-    public Object info;
-    Person03(Object info){ this.info = info; }
-}
+
+
 public class GenericDemo03 {
-    public static void main(String[] args) {
-        Person03 p1 = new Person03("부장");
-        EmployeeInfo ei = (EmployeeInfo)p1.info;
-        System.out.println(ei.rank);
-    }
+
 }
