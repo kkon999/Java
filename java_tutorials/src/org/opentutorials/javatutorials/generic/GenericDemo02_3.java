c96011d5a6718105b6982202e97ba30d19c1139a
kkon999 <gon910910@gmail.com>
2020-03-29 22:54:33 +0900

    제네릭 특징02 - 메소드

--- java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo02_3.java
+++ java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo02_3.java
@@ -20,8 +20,9 @@ public class GenericDemo02_3 {
 
 	public static void main(String[] args) {
 		
-        
-		Integer id = new Integer(10); // Int의 wrapper클래스는 Integer
+        // 제네릭에서는 기본데이터 사용을 하지않고 참조형데이터를 사용해야 한다.
+		// 기본 데이터 타입 Int를 참조형 데이터로 변환해주는 역할인 Integer로 변환
+		Integer id = new Integer(10); 
         
         Person3<EmployeeInfo3, Integer> p1 = new Person3<EmployeeInfo3, Integer>(new EmployeeInfo3(1), id);
         // Person3 클래스의 복수 제네릭을 사용하여  new EmployeeInfo3(1) 과, Intger 객체를 생성하여 호출
