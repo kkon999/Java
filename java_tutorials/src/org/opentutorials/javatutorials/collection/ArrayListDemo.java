c9c26790ec8ff0521214e9b3240ff850c252ef7d
kkon999 <gon910910@gmail.com>
2020-04-01 01:10:21 +0900

    java-컬랙션프레임워크01

---
+++ java_tutorials/src/org/opentutorials/javatutorials/collection/ArrayListDemo.java
@@ -0,0 +1,25 @@
+package org.opentutorials.javatutorials.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	 
+    public static void main(String[] args) {
+        String[] arrayObj = new String[2];
+        arrayObj[0] = "one";
+        arrayObj[1] = "two";
+        // arrayObj[2] = "three"; 오류가 발생한다.
+        for(int i=0; i<arrayObj.length; i++){
+            System.out.println(arrayObj[i]);
+        }
+         
+        ArrayList al = new ArrayList();
+        al.add("one");
+        al.add("two");
+        al.add("three");
+        for(int i=0; i<al.size(); i++){
+            System.out.println(al.get(i));
+        }
+    }
+ 
+}
